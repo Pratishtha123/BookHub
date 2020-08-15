2cd854f433c822d196abeb0cbc765087c33d28f2
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-08-15 21:27:51 +0530

    navigation drawer completed

--- app/src/main/java/com/paru/bookapp/MainActivity.kt
+++ app/src/main/java/com/paru/bookapp/MainActivity.kt
@@ -2,11 +2,54 @@ package com.paru.bookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+lateinit var drawerLayout: DrawerLayout
+lateinit var coordinateLayout: CoordinatorLayout
+lateinit var toolbar: Toolbar
+lateinit var frameLayout: FrameLayout
+lateinit var navigationView: NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout=findViewById(R.id.drawerLayout)
+        coordinateLayout=findViewById(R.id.coordinateLayout)
+        toolbar=findViewById(R.id.toolbar)
+        frameLayout=findViewById(R.id.frameLayout)
+        navigationView=findViewById(R.id.navigationView)
+
+        setUpToolbar()
+
+        val actionBarDrawerToggle=ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+
+    fun setUpToolbar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title="BookHub"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id=item.itemId
+        if(id==android.R.id.home)
+        {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
