363bbc631356b8d545e6c657ad4427684b87d021
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-08-19 17:37:33 +0530

    Profile fragment created

--- app/src/main/java/com/paru/bookapp/activity/WelcomeActivity.kt
+++ app/src/main/java/com/paru/bookapp/activity/WelcomeActivity.kt
@@ -23,6 +23,7 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        sharedpreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
         val isLoggedIn= sharedpreferences.getBoolean("isLoggedIn",false)
 
         if (isLoggedIn)
@@ -38,27 +39,28 @@ class WelcomeActivity : AppCompatActivity() {
         etName=findViewById(R.id.etName)
         etEmail=findViewById(R.id.etEmail)
         btnLogin=findViewById(R.id.btnLogin)
-        sharedpreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
 
-        val name= etName.text.toString()
-        val email= etEmail.text.toString()
-        if(name=="")
-        {
-            Toast.makeText(this@WelcomeActivity,"Please enter Name", Toast.LENGTH_SHORT).show()
-        }
-        else if(email=="")
-        {
-            Toast.makeText(this@WelcomeActivity,"Please enter Email address", Toast.LENGTH_SHORT).show()
-        }
-        else if(!etEmail.text.toString().trim().matches(emailPattern.toRegex()))
-            Toast.makeText(this@WelcomeActivity,"Enter a valid Email Id",Toast.LENGTH_LONG).show()
-        else
-        {
-            sharedpreferences.edit().putString("Name",name).apply()
-            sharedpreferences.edit().putString("Email",email).apply()
-            sharedpreferences.edit().putBoolean("isLoggedIn",true).apply()
-            val intent=Intent(this@WelcomeActivity,MainActivity::class.java)
-            startActivity(intent)
+        btnLogin.setOnClickListener{
+            val name= etName.text.toString()
+            val email= etEmail.text.toString()
+            if(name=="")
+            {
+                Toast.makeText(this@WelcomeActivity,"Please enter Name", Toast.LENGTH_SHORT).show()
+            }
+            else if(email=="")
+            {
+                Toast.makeText(this@WelcomeActivity,"Please enter Email address", Toast.LENGTH_SHORT).show()
+            }
+            else if(!etEmail.text.toString().trim().matches(emailPattern.toRegex()))
+                Toast.makeText(this@WelcomeActivity,"Enter a valid Email Id",Toast.LENGTH_LONG).show()
+            else
+            {
+                sharedpreferences.edit().putString("Name",name).apply()
+                sharedpreferences.edit().putString("Email",email).apply()
+                sharedpreferences.edit().putBoolean("isLoggedIn",true).apply()
+                val intent=Intent(this@WelcomeActivity,MainActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/paru/bookapp/fragment/ProfileFragment.kt
+++ app/src/main/java/com/paru/bookapp/fragment/ProfileFragment.kt
@@ -1,11 +1,20 @@
 package com.paru.bookapp.fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
 import com.paru.bookapp.R
+
+lateinit var sharedPref:SharedPreferences
+lateinit var txtName:TextView
+lateinit var txtMail:TextView
 
 class ProfileFragment : Fragment() {
 
@@ -14,7 +23,15 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view=inflater.inflate(R.layout.fragment_profile, container, false)
+
+        sharedPref= (activity as FragmentActivity).getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+        txtName=view.findViewById(R.id.txtName)
+        txtMail=view.findViewById(R.id.txtMail)
+
+        txtName.text=sharedPref.getString("Name",null)
+        txtMail.text=sharedPref.getString("Email",null)
+        return view
     }
 
 }
